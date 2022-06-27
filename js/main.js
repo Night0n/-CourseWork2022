a0ba429d940c2b98e15fbc23f2bcb53e987c1d9c
Vlad Boholiub <vladislavbogolub4@gmail.com>
2022-06-27 17:49:01 +0300

    зробили перевірку чи виконався запит, і у випадку, якщо він виконався, дістає з обєкту, що прийшов у респонсі дані, які вложені і цікавлять нас(коди країн та ймовірність приналежності до них)

--- js/main.js
+++ js/main.js
@@ -5,6 +5,16 @@ const name = prompt(`Введіть ім'я`, '');
 
 async function search() {
     let response = await fetch(`https://api.nationalize.io?name=${name}`);
-
+    if (response.ok) {
+        let json = await response.json();
+        const countryId1 = json.country[0].country_id;
+        const probability1 = json.country[0].probability;
+        const countryId2 = json.country[1].country_id;
+        const probability2 = json.country[1].probability;
+        const countryId3 = json.country[2].country_id;
+        const probability3 = json.country[2].probability;
+    } else {
+        ourDiv.innerHTML = 'Запит не виконався';
+    }
 }
 search();
