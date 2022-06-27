c9a6757fbdce58e633c8546583a3b8fef80ec3da
Vlad Boholiub <vladislavbogolub4@gmail.com>
2022-06-27 17:19:26 +0300

    Зроблено запит на отримання ймовірності на основі імені

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const ourDiv = document.getElementById('main');
 const name = prompt(`Введіть ім'я`, '');
 
 async function search() {
+    let response = await fetch(`https://api.nationalize.io?name=${name}`);
 
 }
 search();
